e1b1e0999db6284c18b8fbc200669e7d314cfaeb
Damian Sebastian Grigorovič <116663809+Damian591@users.noreply.github.com>
2025-04-05 12:37:42 +0300

    Add files via upload

---
+++ eureka-server/src/test/java/lt/vilniustech/dgrigorovic/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vilniustech.dgrigorovic.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
